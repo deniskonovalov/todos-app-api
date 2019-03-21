dd83e7df77f0ae9a0d69ed633bc107aab2db4bd1
Denis Konovalov <denis.konov93@gmail.com>
2019-03-21 22:41:34 +0200

    Add users/profile GET PATCH DELETE actions

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,4 +52,36 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 });
 
+router.get('/users/profile', auth, async (req, res) => {
+    res.send(req.user);
+});
+
+router.patch('/users/profile', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+router.delete('/users/profile', auth, async (req, res) => {
+    try {
+        await req.user.remove();
+        sendCancelationEmail(req.user.email, req.user.name);
+        res.send(req.user);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
 module.exports = router;
