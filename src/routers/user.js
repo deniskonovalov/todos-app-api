0b36211cf380763c9c96749e5300548fcf61eb10
Denis Konovalov <denis.konov93@gmail.com>
2019-03-21 22:22:55 +0200

    Add /logout and /logoutAll

--- src/routers/user.js
+++ src/routers/user.js
@@ -19,12 +19,36 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        console.log(user);
-
+        
         const token =  await user.generateAuthToken();
         res.send({ user, token });
     } catch (e) {
         res.status(400).send();
+    }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+        res.send();
+
+    } catch (e) {
+        res.send(500);
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+
+    } catch (e) {
+        res.status(500).send();
     }
 });
 
