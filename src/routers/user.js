3dfa34191bc65bca7109fc5f356ac9a9e7233e2a
Denis Konovalov <denis.konov93@gmail.com>
2019-03-21 21:15:42 +0200

    Add Auth middleware and login route

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
     
@@ -8,9 +9,22 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        res.status(200).send();
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
+    }
+});
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        console.log(user);
+
+        const token =  await user.generateAuthToken();
+        res.send({ user, token });
+    } catch (e) {
+        res.status(400).send();
     }
 });
 
