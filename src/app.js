dc0978a5e8c289ac41124bba952bcd439da9154f
Denis Konovalov <denis.konov93@gmail.com>
2019-04-07 15:36:01 +0300

    Add CORS to app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const userRouter = require('./routers/user')
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 app.use(express.json());
 app.use(userRouter)
 
